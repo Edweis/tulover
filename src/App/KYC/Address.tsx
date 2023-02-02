94893979e0b03f4ed91e58cd2137b093f8b6b8fe
François Rullière <kapochamo@gmail.com>
2023-02-02 15:54:29 +0800

    address:

--- src/App/KYC/Address.tsx
+++ src/App/KYC/Address.tsx
@@ -1,24 +1,63 @@
-import { KycData } from './index.js';
+import { useContext } from 'react';
+import InputText from '../../components/InputText.js';
+import Label from '../../components/Label.js';
+import Select from '../../components/Select.js';
+import { kycContext } from './context.js';
+import countries from '../../lib/countries.json';
+import { ButtonAnchor } from '../../components/Button.js';
 
-export default function Address(props: {
-  onSubmit: (data: KycData['address']) => void;
-}) {
+export default function Address(props: { nextUri: string }) {
+  const updateKyc = useContext(kycContext);
   return (
-    <div>
-      <p>address...</p>
-      <button
+    <div className="mb-4 grid gap-8">
+      <h3 className="text-3xl">Address</h3>
+      <form className="grid gap-4">
+        <Label label="Country">
+          <Select>
+            {countries.map((c, i) => (
+              <option value={c.code} key={c.code + i}>
+                {c.name}
+              </option>
+            ))}
+          </Select>
+        </Label>
+        <Label label="Province">
+          <Select>
+            <option>Metro Manila</option>
+          </Select>
+        </Label>
+        <Label label="City">
+          <Select>
+            <option>Manila</option>
+          </Select>
+        </Label>
+        <Label label="Address 1/2">
+          <InputText itemType="text" placeholder="1422 Oroquieta Street 1000" />
+        </Label>
+        <Label label="Address 2/2">
+          <InputText itemType="text" placeholder="#12-43" />
+        </Label>
+        <Label label="Zip code">
+          <InputText itemType="text" placeholder="1000" />
+        </Label>
+      </form>
+      <ButtonAnchor
+        variant="primary"
+        to={props.nextUri}
         onClick={() =>
-          props.onSubmit({
-            city: 'Singapore',
-            country: 'SG',
-            province: 'Singapore',
-            street: '123 road',
-            zip: '12323',
+          updateKyc({
+            address: {
+              city: 'Manila',
+              country: 'PH',
+              province: 'Metro Manila',
+              street: '1422 Oroquieta Street',
+              zip: '12321',
+            },
           })
         }
       >
-        Submit Address
-      </button>
+        Submit Form
+      </ButtonAnchor>
     </div>
   );
 }
