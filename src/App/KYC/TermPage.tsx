85a44abec9672b764da3ad9fda51c12282669f36
François Rullière <kapochamo@gmail.com>
2023-02-06 08:28:38 +0800

    tnc full page

--- src/App/KYC/TermPage.tsx
+++ src/App/KYC/TermPage.tsx
@@ -26,6 +26,21 @@ export default function TermPage() {
         subsequent amendments or supplements thereof and any new laws or rules
         to be promulgated which are applicable to the Deposit Account.{' '}
       </p>
+      <p>
+        <b className="block">1. Deposits.</b>
+        Deposits shall be credited to the Deposit Account after verification by
+        the Bank. The amount verified shall be deemed to be the correct amount
+        deposited by the Accountholder. Any discrepancies noted shall be
+        communicated to the Accountholder.
+      </p>
+      <p>
+        <b className="block">2. Loss or theft of Deposit Item.</b>
+        The Bank shall not be responsible for any payment it may make on any
+        deposit item prior to its notice of loss/ theft of deposit item and such
+        payment shall have the same effects as if made to the Accountholder
+        personally except in case of patently discernable acts of forgery or
+        irregularity.
+      </p>
     </SlideRight>
   );
 }
